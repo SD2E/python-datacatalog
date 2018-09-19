44c34314222cb16f4322ab50ffc6ad28786bcf28
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-09-19 13:23:53 -0500

    Updated README as RST

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="datacatalog",
-    version="0.1.1",
+    version="0.1.2",
     author="Matthew Vaughn",
     author_email="opensource@sd2e.org",
     description="Python package implementing essential logic for SD2 Data Catalog",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/SD2E/python-datacatalog",
     packages=setuptools.find_packages(),
     license="BSD",
