66d82a0c11c16b7d4df3b6a9053ee583cfca431e
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-09-20 17:38:03 -0500

    update collection names as per discussion w @ngaffney

--- datacatalog/configs.py
+++ datacatalog/configs.py
@@ -18,8 +18,8 @@ class CatalogStore(object):
     products_dir = Constants.PRODUCTS_ROOT
     references_dir = Constants.REFERENCES_ROOT
     collections = {'updates': 'updates',
-                   'fixity': 'files-fixity',
-                   'files': 'files',
+                   'fixity': 'files',
+                   'files': 'datafiles',
                    'challenges': 'challenges',
                    'experiments': 'experiments',
                    'samples': 'samples',
@@ -29,8 +29,8 @@ class CatalogStore(object):
                    'tokens': 'tokens',
                    'products_files': 'products_files',
                    'products_files_fixity': 'products_files_fixity',
-                   'inputs_files': 'inputs_files',
-                   'inputs_files_fixity': 'inputs_files_fixity'}
+                   'reference_files': 'reference_files',
+                   'reference_files_fixity': 'reference_files_fixity'}
     batch = 1000
     mongodb = {'host': 'catalog.sd2e.org',
         'port': '27020', 'username': None,
