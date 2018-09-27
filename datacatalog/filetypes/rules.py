4cc3fd96cd16d3334666d916d4d4d1bdd82e7754
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-09-27 06:23:50 -0500

    Additional file types: Add stderr and stdout to LOG; FASTQC; MULTIQC; GIT; FASTA; ENV.

--- datacatalog/filetypes/rules.py
+++ datacatalog/filetypes/rules.py
@@ -4,6 +4,11 @@ from .filetype import FileType, FileTypeError
 # TODO: Protein Mass Spec bestiary https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3518119/
 # TODO: Replace rules with local additions to local xdg mime catalog
 FILETYPES = [
+    ('LOG', 'Log file', ['\.err$', '\.out$']),
+    ('ENV', 'Environment file', ['\.env$', '\.rc$']),
+    ('FASTQC', 'FASTQC outputs', ['fastqc\.html$', 'fastqc\.zip$']),
+    ('MULITIQC', 'FASTQC outputs', ['multiqc_report\.html$']),
+    ('FASTA', 'FASTA sequence file', ['\.fa$', '\.fasta$', '\.fa.gz$', '\.fasta.gz$']),
     ('BAM', 'Binary SAM', ['\.bam$']),
     ('VCF', 'Variant Call Format', ['\.vcf$']),
     ('BCF', 'Binary Variant Call Format', ['\.bcf$']),
@@ -14,11 +19,12 @@ FILETYPES = [
     ('SRAW', 'Raw proteomics file', ['\.sraw$']),
     ('MZML', 'Proteomics mzML file', ['\.mzML$']),
     ('MSF', 'Magellan storage file', ['\.msf$']),
-    ('SAMPLES', 'SD2 Samples Metadata JSON', ['^metadata-[0-9-]+.json$']),
+    ('SAMPLES', 'SD2 Samples Metadata JSON', ['^metadata-[a-z0-9-]+.json$']),
     ('BPROV', 'Biofab Provenance JSON', ['^provenance_dump.json$']),
     ('INI', 'INI config file', ['\.ini$']),
     ('SECRETS', 'Abaco secrets file', ['^secrets.json$']),
-    ('CONFIG', 'Config file', ['reactor.rc', 'config.yml$']),
+    ('CONFIG', 'Config file', ['config.rc$', 'reactor.rc$', 'config.yml$']),
+    ('GIT', 'Git file', ['\.git']),
     ('JENKINS', 'Jenkins Pipeline file', ['^Jenkinsfile$']),
     ('DOCKERFILE', 'Docker build file', ['^Dockerfile$']),
     ('REQUIREMENTS', 'Python requirements file', ['^requirements.txt$']),
