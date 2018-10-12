1ebdfa59d74bed7872d9b011b1eb5fa10dc60916
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-10-12 18:00:33 -0500

    Stub for exposing hand-curated classifier schemas for lab sample formats

---
+++ datacatalog/formats/schemas.py
@@ -0,0 +1,11 @@
+from jsonschemas import JSONSchemaBaseObject
+
+def get_schemas():
+    return dict()
+    # labels = listall_labels()
+    # setup_args = {'_filename': 'filetype_label',
+    #              'title': 'File Type Label',
+    #              '_type': 'string',
+    #              'enum' : labels}
+    # schema = JSONSchemaBaseObject(**setup_args).to_jsonschema()
+    # return {'filetype_label': schema}
