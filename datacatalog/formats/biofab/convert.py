cb7d92a2b3d39a1fe2f1e160d6a572b505848858
mweston <mark.william.weston@gmail.com>
2018-11-10 10:53:53 -0500

    schema sync and converter updates

--- datacatalog/formats/biofab/convert.py
+++ datacatalog/formats/biofab/convert.py
@@ -6,5 +6,5 @@ class Biofab(Converter):
     def convert(self, input_fp, output_fp=None, verbose=True, config={}, enforce_validation=True):
         # schema_file, input_file, verbose=True, output=True, output_file=None
         from .runner import convert_biofab
-        passed_config = config if config != {} else self.get('options', {})
+        passed_config = config if config != {} else self.options
         return convert_biofab(self.targetschema, input_fp, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
--- datacatalog/formats/ginkgo/convert.py
+++ datacatalog/formats/ginkgo/convert.py
@@ -5,5 +5,5 @@ class Ginkgo(Converter):
     def convert(self, input_fp, output_fp=None, verbose=True, config={}, enforce_validation=True):
         # schema_file, input_file, verbose=True, output=True, output_file=None
         from .runner import convert_ginkgo
-        passed_config = config if config != {} else self.get('options', {})
+        passed_config = config if config != {} else self.options
         return convert_ginkgo(self.targetschema, input_fp, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
