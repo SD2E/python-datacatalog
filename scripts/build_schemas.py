421de880f118098309b43556e656ff335eab5efe
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-11 09:17:29 -0600

    Add support for --filters

--- scripts/build_schemas.py
+++ scripts/build_schemas.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tempfile
+import argparse
 
 HERE = os.getcwd()
 THIS = os.path.dirname(__file__)
@@ -8,20 +9,27 @@ PARENT = os.path.dirname(THIS)
 sys.path.append(PARENT)
 import datacatalog
 
-def regenerate():
+def regenerate(filters=None):
     if datacatalog.config.get_osenv_bool('MAKETESTS'):
         DESTDIR = tempfile.mkdtemp()
     else:
         DESTDIR = os.path.join(PARENT, 'schemas')
 
-    for fname, schema in datacatalog.jsonschemas.get_all_schemas().items():
+    for fname, schema in datacatalog.jsonschemas.get_all_schemas(filters).items():
         destpath = os.path.join(DESTDIR, fname + '.json')
         with open(destpath, 'w+') as j:
             j.write(schema)
     return True
 
 def main():
-    regenerate()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filter", help="Comma-separated list of JSONschema packages")
+    args = parser.parse_args()
+    if args.filter:
+        filters = args.filter.split(',')
+    else:
+        filters = None
+    regenerate(filters)
 
 if __name__ == '__main__':
     main()
