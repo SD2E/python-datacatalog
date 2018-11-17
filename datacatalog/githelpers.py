a1b8e0fb45682609e7295406868b8b0bc8c5aeee
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-16 18:01:42 -0600

    Document githelpers

--- datacatalog/githelpers.py
+++ datacatalog/githelpers.py
@@ -3,20 +3,43 @@ import subprocess
 import git
 
 def get_sha1_short(repo=os.getcwd()):
+    """Return abbreviated SHA1 hash for the specified git repository
+
+    Returns the initial seven characters of the current repository's most
+    recent git commit.
+
+    Args:
+        repo (str, optional): The directory to inspect for a git reflog
+
+    Returns:
+        str: A hexadecimal string representing the SHA1 hash
+    """
     return get_sha1(repo)[0:7]
 
-# TODO - Replace with native method from GitPython
 def get_sha1(repo=os.getcwd()):
-    """
-    Grabs the current SHA-1 hash of the given directory's git HEAD-revision.
-    The output of this is equivalent to calling git rev-parse HEAD.
+    """Get SHA-1 hash of given git repository
 
-    Be aware that a missing git repository will make this return an error message,
-    which is not a valid hash.
+    Inspects the specified directory, assumed to be a git repository, and
+    returns the SHA1 hash from its `HEAD-revision`. The output is equivalent to
+    calling `git rev-parse HEAD`.
+
+    Args:
+        repo (str, optional): The directory to inspect for a git reflog
+
+    Returns:
+        str: A hexadecimal string representing the SHA1 hash
+
+    Todo:
+        Replace with native GitPython code
     """
     sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=repo)
     return sha.decode('ascii').strip()
 
 def get_remote_uri():
+    """Gets the remote origin for the current directory
+
+    Returns:
+        str: an SSH or HTTP git repository URL
+    """
     repo = git.Repo(".", search_parent_directories=True)
     return repo.remote("origin").url
