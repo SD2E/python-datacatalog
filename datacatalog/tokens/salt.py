55ea3158801bc1d323742ae59e7837c841d63d77
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-18 11:12:16 -0600

    Token and Salt classes

--- datacatalog/tokens/salt.py
+++ datacatalog/tokens/salt.py
@@ -1,6 +1,21 @@
 import os
 import base64
 
-def generate_salt():
-    salt = os.urandom(16)
-    return str(base64.b64encode(salt).decode('utf-8'))
+SALT_LENGTH = 16
+"""Number of bytes in the salt"""
+class Salt(str):
+    def __new__(cls, value):
+        value = str(value).lower()
+        return str.__new__(cls, value)
+
+def generate_salt(length=SALT_LENGTH):
+    """Generates a cryptographic salt
+
+    Args:
+        length (int, optional): Length of salt in bytes
+
+    Returns:
+        Salt: a salt value
+    """
+    salt = os.urandom(length)
+    return Salt(base64.b64encode(salt).decode('utf-8'))
--- datacatalog/tokens/token.py
+++ datacatalog/tokens/token.py
@@ -2,15 +2,32 @@ import hashlib
 import base64
 
 TOKEN_LENGTH = 16
+"""Generated token length"""
 
 from ..debug_mode import debug_mode
 
+class Token(str):
+    def __new__(cls, value):
+        value = str(value).lower()
+        return str.__new__(cls, value)
+
 def get_token(salt, *args):
+    """Deterministically generates a token from an argument set
+
+    Tokens provide a minimal signature that can be used to authorize edits
+    to a record managed by datacatalog.
+
+    Args:
+        salt (str):
+
+    Returns:
+        str: An alphanumeric Token
+    """
     argset = [salt]
     argset.extend(args)
     str_argset = [str(a) for a in argset if True]
     msg = ':'.join(str_argset)
-    return str(hashlib.sha256(msg.encode('utf-8')).hexdigest()[0:TOKEN_LENGTH])
+    return Token(hashlib.sha256(msg.encode('utf-8')).hexdigest()[0:TOKEN_LENGTH])
 
 def validate_token(token, salt, *args):
     if debug_mode() is True:
