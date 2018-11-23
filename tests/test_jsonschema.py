1c876fe63bb0f13e6d0783783e47c5ba595e1c08
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-23 16:43:45 -0600

    Bringing in schema changes from @mweston samples-schema.json

--- tests/test_jsonschema.py
+++ tests/test_jsonschema.py
@@ -21,8 +21,9 @@ def resolver(base_uri=JSONSCHEMA_BASE_URL, schema='sample_set'):
     print('REMOTE_URI', remote_uri)
     return jsonschema.RefResolver('', '').resolve_remote(remote_uri)
 
-@pytest.mark.parametrize("jsonfile", [('samples-biofab.json'), ('samples-ginkgo.json'), ('samples-transcriptic.json')])
+@pytest.mark.parametrize("jsonfile", [('17016_87542.json'), ('20249.json'), ('95463.json'), ('Novelchassis_Nand_gate_controls.json'), ('Novelchassis_Nand_gate_samples.json'), ('r1btfp5k2edgn_r1btpym75nsdh_samples.json'), ('r1bzc55fpurbj.json'), ('samples_nc.json')])
 def test_validate_sample_set(jsonfile):
+    print('TEST', jsonfile)
     res = resolver(schema='sample_set')
     jsonpath = os.path.join(DATA_DIR, jsonfile)
     instance = json.load(open(jsonpath, 'r'))
