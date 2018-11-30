0dddf06e171a7af7e2dfc167f07eddf785a7dca4
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-30 16:12:22 -0600

    Rationalize imports for formats.sample_attributes

--- datacatalog/formats/sample_attributes/runner.py
+++ datacatalog/formats/sample_attributes/runner.py
@@ -3,18 +3,18 @@ import json
 import sys
 import os
 import six
+import re
 
-from jsonschema import validate
-from jsonschema import ValidationError
-# Hack hack
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from common import SampleConstants
-from common import namespace_sample_id, namespace_file_id, namespace_measurement_id, namespace_lab_id, create_media_component, create_mapped_name, create_value_unit, map_experiment_reference, namespace_experiment_id
+from jsonschema import validate, ValidationError
+from sbol import *
 from synbiohub_adapter.query_synbiohub import *
 from synbiohub_adapter.SynBioHubUtil import *
-from sbol import *
-from datacatalog.agavehelpers import AgaveHelper
-import re
+
+from ..agavehelpers import AgaveHelper
+from .common import SampleConstants
+from .common import namespace_file_id, namespace_sample_id, namespace_measurement_id, namespace_lab_id, create_media_component, create_mapped_name, create_value_unit, map_experiment_reference, namespace_experiment_id
+from .mappings import SampleContentsFilter
+
 
 attributes_attr = "attributes"
 files_attr = "files"
@@ -47,12 +47,12 @@ def convert_sample_attributes(schema_file, input_file, verbose=True, output=True
         # try to figure out the lab, experiment_id, cp, etc from the filename
         if lab is None and attributes_attr in sample_attributes_sample and files_attr in sample_attributes_sample[attributes_attr]:
             for file in sample_attributes_sample[attributes_attr][files_attr]:
-                
+
                 # lab mapping
                 if "transcriptic" in file and SampleConstants.LAB not in output_doc:
                     lab = SampleConstants.LAB_TX
                     output_doc[SampleConstants.LAB] = lab
-                
+
                 # CP mapping
                 if "yeast-gates" in file and SampleConstants.CHALLENGE_PROBLEM not in output_doc:
                     output_doc[SampleConstants.CHALLENGE_PROBLEM] = SampleConstants.CP_YEAST_GATES
@@ -69,10 +69,10 @@ def convert_sample_attributes(schema_file, input_file, verbose=True, output=True
 
         if lab is None:
             raise ValueError("Could not parse lab from sample {}".format(sample_attributes_sample))
-    
+
         sample_doc[SampleConstants.SAMPLE_ID] = namespace_sample_id(str(sample_attributes_sample["sample"]), lab)
 
-        #TODO
+        # TODO
         # Contents
         # Media, replicate, timepoint, temperature etc.
         # Measurements and files
@@ -83,7 +83,7 @@ def convert_sample_attributes(schema_file, input_file, verbose=True, output=True
     try:
         validate(output_doc, schema)
         # if verbose:
-        #print(json.dumps(output_doc, indent=4))
+        # print(json.dumps(output_doc, indent=4))
         if output is True or output_file is not None:
             if output_file is None:
                 path = os.path.join(
