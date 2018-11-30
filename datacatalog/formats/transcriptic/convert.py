afa4a929ef3e1d64fd3411df4bd4c776ac4d8fda
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-11-30 15:44:51 -0600

    Harmonized docs and global attributes for each Converter

--- datacatalog/formats/transcriptic/convert.py
+++ datacatalog/formats/transcriptic/convert.py
@@ -1,12 +1,17 @@
 import sys
 from ..converter import Converter, ConversionError, ValidationError
-# from .runner import convert_transcriptic
 
 class Transcriptic(Converter):
+    """Convert Transcriptic samples.json to sample-set schema"""
+
     VERSION = '0.0.1'
     FILENAME = 'transcriptic_samples'
 
     def convert(self, input_fp, output_fp=None, verbose=True, config={}, enforce_validation=True):
+        """Do the conversion by running a method in runner.py"""
         from .runner import convert_transcriptic
         passed_config = config if config != {} else self.options
-        return convert_transcriptic(self.targetschema, input_fp, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
+        return convert_transcriptic(self.targetschema, input_fp,
+                                    verbose=verbose,
+                                    config=passed_config,
+                                    output_file=output_fp, enforce_validation=enforce_validation)
