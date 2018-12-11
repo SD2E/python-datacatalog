837ead64ab398cc4936d03dc426790f1aebf23d8
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-12-11 10:59:24 -0600

    Added new PipelineJob event 'resource' which is meant to be invoked when the underlying system is marshaling resources. This allows a distinction between purely informative 'update' events and this class of more informative notifications

--- tests/data/pipelinejob/__init__.py
+++ tests/data/pipelinejob/__init__.py
@@ -1,5 +1,8 @@
 CASES = [('tacobot.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True)]
-EVENTS = [('tacobot-run.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True), ('tacobot-update.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True), ('tacobot-finish.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True)]
+EVENTS = [('tacobot-run.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True),
+          ('tacobot-update.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True),
+          ('tacobot-resource.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True),
+          ('tacobot-finish.json', '10797ce0-c130-5738-90d5-9e854adc67dd', True)]
 
 FAILED_EVENTS = [('tacobot-run-wrong-uuid.json', '10797ce0-d130-a738-60d5-9e854adc67dd', True)]
 
