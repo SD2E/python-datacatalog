c3419765c3367617f789dae6c34a91b158818323
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-12-14 12:21:37 -0600

    Merge in mongo.manage_views

--- datacatalog/mongo/manage_views.py
+++ datacatalog/mongo/manage_views.py
@@ -7,32 +7,32 @@ import urllib
 Methods for managing views:
 
 createView(idb, view_name, view_definition) - creates a view in the idb database
-getView(idb,view_name) - gets the json representation of the view from the DB 
+getView(idb,view_name) - gets the json representation of the view from the DB
 drop(view(idb,view_name) - drops the view (has to be a view)
 
 """
 
 def createView(idb, view_name, view_defnition):
-    viewOn=view_definition["view_on"]
-    pipeline=view_definition["pipeline"]
+    viewOn = view_definition["view_on"]
+    pipeline = view_definition["pipeline"]
     idb.command('create', view_name, viewOn=viewOn, pipeline=pipeline)
 
-def getView(idb,view_name):
-    return idb.command('listCollections', filter={'name':view_name,'type':"view"})
+def getView(idb, view_name):
+    return idb.command('listCollections', filter={'name': view_name, 'type': "view"})
 
-def dropView(idb,view_name):
-    test=getView(idb,view_name)
+def dropView(idb, view_name):
+    test = getView(idb, view_name)
     if len(test['cursor']['firstBatch']) == 0:
         raise Exception("No View of name " + view_name)
-    view=idb[view_name]
+    view = idb[view_name]
     view.drop()
 
 
 def test(db):
 
-    viewName="testview_do_not_use"
-    viewon_table="science_view"
-    viewjson_text="""{
+    viewName = "testview_do_not_use"
+    viewon_table = "science_view"
+    viewjson_text = """{
       "view_on":viewon_table,
       "pipeline":[{"$project": {"challenge_problem": "$challenge_problem",
         "experiment_reference_url": "$experiment.experiment_reference_url",
@@ -70,14 +70,13 @@ def test(db):
         "timepoint": "$measurement.timepoint",
         "normalization": "$file.attributes.normalization",
         "library_prep": "$file.attributes.library_prep"}}]"""
-    
-    pprint.pprint(getView(db,viewName))
 
-    createView(db,viewName,json.loads(viewjson_text))
+    pprint.pprint(getView(db, viewName))
 
-    pprint.pprint(getView(db,viewName))
+    createView(db, viewName, json.loads(viewjson_text))
 
-    dropView(db,viewName)
+    pprint.pprint(getView(db, viewName))
 
-    pprint.pprint(getView(db,viewName))
+    dropView(db, viewName)
 
+    pprint.pprint(getView(db, viewName))
