1aae151c9b8b8f6b24a412440ff4d451c8197e39
gzheng2 <gzheng@eagc.test>
2019-01-09 10:48:50 -0500

    load email and token from passwd_config

--- datacatalog/formats/sample_attributes/convert.py
+++ datacatalog/formats/sample_attributes/convert.py
@@ -6,7 +6,7 @@ class SampleAttributes(Converter):
         # schema_file, input_file, verbose=True, output=True, output_file=None
         from .runner import convert_sample_attributes
         passed_config = config if config != {} else self.options
-        tx_credential = json.load(open("tx_credential.json"))
-        email = tx_credential['EMAIL']
-        token = tx_credential['TOKEN']
-        return convert_sample_attributes(self.targetschema, self.encoding, input_fp, email, token, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
+        tx_email = passed_config['tx_email']
+        tx_token = passed_config['tx_token']
+        print("***tx_email: {} tx_token: {}".format(tx_email, tx_token))
+        return convert_sample_attributes(self.targetschema, self.encoding, input_fp, tx_email, tx_token, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
