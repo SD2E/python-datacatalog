0979fa01a09c5eb3f59baed2c9cb25fd1c87e513
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-02-05 11:13:28 -0600

    job.uuid now includes contents of child_of. Updated unit tests to reflect new generated UUID values

--- tests/data/pipelinejob/__init__.py
+++ tests/data/pipelinejob/__init__.py
@@ -1,8 +1,8 @@
-CASES = [('tacobot.json', '107f299e-c477-51d7-93d1-b01cdf18674a', True)]
-EVENTS = [('tacobot-run.json', '107f299e-c477-51d7-93d1-b01cdf18674a', True),
-          ('tacobot-update.json', '107f299e-c477-51d7-93d1-b01cdf18674a', True),
-          ('tacobot-resource.json', '107f299e-c477-51d7-93d1-b01cdf18674a', True),
-          ('tacobot-finish.json', '107f299e-c477-51d7-93d1-b01cdf18674a', True)]
+CASES = [('tacobot.json', '107b93f3-1eae-5e79-8a18-0a480f8aa3a5', True)]
+EVENTS = [('tacobot-run.json', '107b93f3-1eae-5e79-8a18-0a480f8aa3a5', True),
+          ('tacobot-update.json', '107b93f3-1eae-5e79-8a18-0a480f8aa3a5', True),
+          ('tacobot-resource.json', '107b93f3-1eae-5e79-8a18-0a480f8aa3a5', True),
+          ('tacobot-finish.json', '107b93f3-1eae-5e79-8a18-0a480f8aa3a5', True)]
 
 FAILED_EVENTS = [('tacobot-run-wrong-uuid.json', '1070761d-1a7a-535e-b6d3-a1a728a249ce', True)]
 
