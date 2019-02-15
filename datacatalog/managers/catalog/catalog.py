24021f1f0d9bb503858eb33f40f57952ded15f90
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-02-15 13:30:27 -0600

    Added self_from_ids(id_list...) to common.Manager to quickly resolve identifiers into UUIDs with optional type enforcement.

--- datacatalog/managers/catalog/catalog.py
+++ datacatalog/managers/catalog/catalog.py
@@ -18,64 +18,64 @@ class CatalogManagerError(ManagerError):
 class CatalogManager(Manager):
     """Supports operations spanning multiple LinkedStore collections"""
 
-    def get_uuidtype(self, uuid):
-        """Identify the named type for a given UUID
+    # def get_uuidtype(self, uuid):
+    #     """Identify the named type for a given UUID
 
-        Args:
-            uuid (str): UUID to classify by type
+    #     Args:
+    #         uuid (str): UUID to classify by type
 
-        Returns:
-            str: Named type of the UUID
-        """
-        typeduuid.validate(uuid)
-        return typeduuid.get_uuidtype(uuid)
+    #     Returns:
+    #         str: Named type of the UUID
+    #     """
+    #     typeduuid.validate(uuid)
+    #     return typeduuid.get_uuidtype(uuid)
 
-    def get_by_uuid(self, uuid):
-        """Returns a LinkedStore document by UUID
+    # def get_by_uuid(self, uuid):
+    #     """Returns a LinkedStore document by UUID
 
-        Args:
-            uuid (str): UUID of the document to retrieve
+    #     Args:
+    #         uuid (str): UUID of the document to retrieve
 
-        Returns:
-            dict: The document that was retrieved
-        """
-        storename = self.get_uuidtype(uuid)
-        return self.sanitize(
-            self.stores[storename].find_one_by_uuid(uuid))
+    #     Returns:
+    #         dict: The document that was retrieved
+    #     """
+    #     storename = self.get_uuidtype(uuid)
+    #     return self.sanitize(
+    #         self.stores[storename].find_one_by_uuid(uuid))
 
-    def get_by_identifier(self, identifier_string):
-        """Search LinkedStores for a string identifier
+    # def get_by_identifier(self, identifier_string):
+    #     """Search LinkedStores for a string identifier
 
-        Args:
-            identifier_string (str): An identifier string
-        Returns:
-            dict: The document that was retrieved
-        """
-        # TODO - use any namespacing in identifier_string to select intitial store to query
-        for sname, store in self.stores.items():
-            for i in store.get_identifiers():
-                query = {i: identifier_string}
-                resp = store.coll.find_one(query)
-                if resp is not None:
-                    return self.sanitize(resp)
-        return None
+    #     Args:
+    #         identifier_string (str): An identifier string
+    #     Returns:
+    #         dict: The document that was retrieved
+    #     """
+    #     # TODO - use any namespacing in identifier_string to select intitial store to query
+    #     for sname, store in self.stores.items():
+    #         for i in store.get_identifiers():
+    #             query = {i: identifier_string}
+    #             resp = store.coll.find_one(query)
+    #             if resp is not None:
+    #                 return self.sanitize(resp)
+    #     return None
 
-    def get_by_uuids(self, uuids):
-        """Returns a list of LinkedStore documents by UUID
+    # def get_by_uuids(self, uuids):
+    #     """Returns a list of LinkedStore documents by UUID
 
-        Args:
-            uuids (list): List of document UUIDs
+    #     Args:
+    #         uuids (list): List of document UUIDs
 
-        Returns:
-            list The document that was retrieved
-        """
-        recs = list()
-        for uuid in uuids:
-            resp = self.get_by_uuid(uuid)
-            if resp is not None:
-                recs.append(resp)
-        sorted_recs = sorted(recs, key=lambda k: k['uuid'])
-        return sorted_recs
+    #     Returns:
+    #         list The document that was retrieved
+    #     """
+    #     recs = list()
+    #     for uuid in uuids:
+    #         resp = self.get_by_uuid(uuid)
+    #         if resp is not None:
+    #             recs.append(resp)
+    #     sorted_recs = sorted(recs, key=lambda k: k['uuid'])
+    #     return sorted_recs
 
     def delete_by_uuid(self, uuid, token=None):
         """Deletes LinkedStore document and all linked references
