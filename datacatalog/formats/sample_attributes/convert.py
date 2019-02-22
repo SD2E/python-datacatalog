8a5ec358bcfa8b93c0c7b731fe1e119fc31ca2fb
mweston <mark.william.weston@gmail.com>
2019-02-22 11:42:35 -0500

    Improve TX API access, show TX API response

--- datacatalog/formats/sample_attributes/convert.py
+++ datacatalog/formats/sample_attributes/convert.py
@@ -6,7 +6,6 @@ class SampleAttributes(Converter):
         # schema_file, input_file, verbose=True, output=True, output_file=None
         from .runner import convert_sample_attributes
         passed_config = config if config != {} else self.options
-        tx_email = passed_config['tx_email']
-        tx_token = passed_config['tx_token']
-        print("***tx_email: {} tx_token: {}".format(tx_email, tx_token))
+        tx_email = passed_config['tx']['email']
+        tx_token = passed_config['tx']['token']
         return convert_sample_attributes(self.targetschema, self.encoding, input_fp, tx_email, tx_token, verbose=verbose, config=passed_config, output_file=output_fp, enforce_validation=enforce_validation)
