deeccb0cc10a546f6a8f3a34cd49b309c1c9b2c1
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-03-02 14:25:33 -0600

    filetypes.mime now defers immediately to filename-based typing. It also assumes that the existence of the file has been validated. This saves several implicit, redundant calls to os.stat().

--- datacatalog/filetypes/mime.py
+++ datacatalog/filetypes/mime.py
@@ -1,3 +1,4 @@
+import stat
 from xdg import Mime
 from .filetype import FileType, FileTypeError
 from .unknown import UnknownFileType
@@ -16,7 +17,7 @@ def infer(filename):
     Returns:
         FileType: What kind of file it is
     """
-    mime = Mime.get_type2(filename)
+    mime = get_type_optimized(filename)
     # print('MIME', mime)
     if str(mime) == 'application/octet-stream':
         return UnknownFileType()
@@ -33,3 +34,38 @@ def listall():
     for t in Mime.types.items():
         alltypes.append(FileType(mimetype=t[1]))
     return alltypes
+
+def get_type_optimized(path, follow=False):
+    mtypes = sorted(Mime.globs.all_matches(path),
+                    key=(lambda x: x[1]), reverse=True)
+    if mtypes:
+        max_weight = mtypes[0][1]
+        i = 1
+        for mt, w in mtypes[1:]:
+            if w < max_weight:
+                break
+            i += 1
+        mtypes = mtypes[:i]
+        if len(mtypes) == 1:
+            return mtypes[0][0]
+
+        possible = [mt for mt, w in mtypes]
+    else:
+        possible = None
+
+    # Break if we have a named-based candidate
+    if mtypes:
+        return mtypes[0][0]
+
+    t = None
+    try:
+        t = Mime.magic.match(path, possible=possible)
+    except IOError:
+        t = None
+
+    if t:
+        return t
+    elif mtypes:
+        return mtypes[0][0]
+    else:
+        return Mime.text if Mime.is_text_file(path) else Mime.octet_stream
