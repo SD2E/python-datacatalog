c07d5101e10f4e266656b3f90e3af9d2da40e6f3
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-03-02 14:37:41 -0600

    Unit tests added to assist FixityIndexer.index profiling

--- tests/test_107_stores_fixity.py
+++ tests/test_107_stores_fixity.py
@@ -3,6 +3,8 @@ import pytest
 import sys
 import yaml
 import json
+import time
+import tempfile
 from pprint import pprint
 from . import longrun, delete
 
@@ -15,6 +17,7 @@ import datacatalog
 from .data.fixity import files
 from datacatalog.linkedstores.fixity import FixityStore
 from datacatalog.linkedstores.file import FileStore
+from datacatalog.linkedstores.fixity.indexer import FixityIndexer
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 PARENT = os.path.dirname(HERE)
@@ -83,8 +86,83 @@ def test_fixity_normpath(mongodb_settings, filename, fuuid):
     identifier_string_uuid = base.get_typeduuid(filename, binary=False)
     assert identifier_string_uuid == fuuid
 
+@longrun
+def test_fixity_indexer_cache(monkeypatch):
+    """Confirms that caching os.stats() makes FixityIndexer faster
+    """
+    monkeypatch.setenv('DEBUG_STORES_NATIVE_PREFIX', DATA_DIR)
+    # Run with cache ON
+    elapsed = list()
+    for use_cache in [False, True]:
+        start_time = time.time()
+        for reps in range(0, 2000):
+            for fname, cksum, tsize, ftype in files.NO_SURPRISES:
+                abs_fname = os.path.join(DATA_DIR, fname)
+                rel_fname = os.path.join('tests/data/fixity/files', fname)
+                fidx = FixityIndexer(abs_filename=os.path.abspath(abs_fname), name=rel_fname, cache_stat=use_cache)
+                fidx.sync()
+                assert fidx.type == ftype, 'wrong type for {}'.format(fname)
+        elapsed.append(time.time() - start_time)
+    assert elapsed[0] > elapsed[1], 'Avoiding stat() had no apparent effect'
+
+@longrun
+def test_fixity_indexer_cache_no_buffering(monkeypatch):
+    """Confirms os.stats() cache makes FixityIndexer faster without file.read buffering
+    """
+    TMPDIR = tempfile.mkdtemp()
+    FILE_SIZE = 65536000
+    elapsed = list()
+    for use_cache in [False, True]:
+        start_time = time.time()
+        for fname, cksum, tsize, ftype in files.NO_SURPRISES:
+            abs_fname = os.path.join(TMPDIR, fname)
+            fout = open(abs_fname, 'wb', 0)
+            fout.write(os.urandom(FILE_SIZE))
+            fout.close()
+            fidx = FixityIndexer(abs_filename=abs_fname, name=fname, cache_stat=use_cache)
+            fidx.sync()
+            os.unlink(abs_fname)
+        elapsed.append(time.time() - start_time)
+    assert elapsed[0] > elapsed[1], 'Avoiding stat() had no apparent effect'
+
+@longrun
+def test_fixity_indexer_cache_blocksize(monkeypatch):
+    """This profiles the impact of block size on fixity operations
+
+    It may fail idiopathicallydue to local OS or host operations that consume
+    disk and CPU IO.
+    """
+    TMPDIR = tempfile.mkdtemp()
+    FILE_SIZE = 32000000
+    REPS = 5
+    BLOCK_SIZES = [8000, 16000, 32000, 64000, 128000, 256000, 512000]
+    elapsed = list()
+    for rep in REPS:
+        rep_elapsed = list()
+        for block_size in BLOCK_SIZES:
+            start_time = time.time()
+            for fname, cksum, tsize, ftype in files.NO_SURPRISES:
+                abs_fname = os.path.join(TMPDIR, fname)
+                fout = open(abs_fname, 'wb', 0)
+                fout.write(os.urandom(FILE_SIZE))
+                fout.close()
+                fidx = FixityIndexer(abs_filename=abs_fname, name=fname,
+                                     cache_stat=True, block_size=block_size)
+                fidx.sync()
+                os.unlink(abs_fname)
+            rep_elapsed.append((block_size, time.time() - start_time))
+        elapsed.append(rep_elapsed)
+    prev_elapse = -1
+    deltas = list()
+    for buf, elapse in elapsed:
+        if prev_elapse > 0:
+            pct_delta = 100 * (1 - (elapse - prev_elapse) / prev_elapse)
+            assert pct_delta > 100, 'No more ROI on buffer size beyond {}'.format(buf)
+            deltas.append((buf, pct_delta))
+        prev_elapse = elapse
+
 @delete
-def test_delete_fixity(mongodb_settings):
+def test_fixity_delete(mongodb_settings):
     fixity_store = FixityStore(mongodb_settings)
     for fname, cksum, tsize, ftype in files.TESTS:
         doc = fixity_store.find_one_by_id(name=fname)
