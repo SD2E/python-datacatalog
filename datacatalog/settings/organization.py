231263af2d9930772e7c4b2dace9911ea313c6ce
vaughn <vaughn@tacc.utexas.edu>
2019-03-05 10:44:32 -0600

    Update namespacing of organization.py variables as they should not be Data Catalog-specific

--- datacatalog/settings/organization.py
+++ datacatalog/settings/organization.py
@@ -5,22 +5,21 @@ __all__ = ['DATE_FORMAT', 'DNS_DOMAIN', 'TACC_PROJECT_NAME', 'TACC_PROJECT_ID',
            'TACC_PROJECT_GROUP', 'TACC_MANAGER_ACCOUNT', 'TACC_TENANT',
            'TACC_API_SERVER', 'TACC_PRIMARY_STORAGE_SYSTEM', 'TACC_JUPYTER_SERVER']
 
-DATE_FORMAT = os.environ.get("CATALOG_DATE_FORMAT", "YYYYMMDDTHHmmssZZ")
-
-DNS_DOMAIN = os.environ.get('CATALOG_DNS_DOMAIN', 'sd2e.org')
+DATE_FORMAT = os.environ.get("DATE_FORMAT", "YYYYMMDDTHHmmssZZ")
+DNS_DOMAIN = os.environ.get('DNS_DOMAIN', 'sd2e.org')
 
 # TACC.cloud TAS
-TACC_PROJECT_NAME = os.environ.get('CATALOG_TACC_PROJECT_NAME', 'SD2E-Community')
-TACC_PROJECT_ID = os.environ.get('CATALOG_TACC_PROJECT_ID', '37391')
-TACC_PROJECT_GROUP = os.environ.get('CATALOG_TACC_PROJECT_GROUP', '819382')
+TACC_PROJECT_NAME = os.environ.get('TACC_PROJECT_NAME', 'SD2E-Community')
+TACC_PROJECT_ID = os.environ.get('TACC_PROJECT_ID', '37391')
+TACC_PROJECT_GROUP = os.environ.get('TACC_PROJECT_GROUP', '819382')
 
 # TACC.cloud Agave/Abaco/Jupyter
-TACC_TENANT = os.environ.get('CATALOG_TACC_TENANT', 'sd2e')
-TACC_MANAGER_ACCOUNT = os.environ.get('CATALOG_TACC_MANAGER_ACCOUNT', 'sd2eadm')
+TACC_TENANT = os.environ.get('TACC_TENANT', 'sd2e')
+TACC_MANAGER_ACCOUNT = os.environ.get('TACC_MANAGER_ACCOUNT', 'sd2eadm')
 TACC_API_SERVER = os.environ.get(
-    'CATALOG_TACC_API_SERVER', 'https://api.' + DNS_DOMAIN + '/')
+    'TACC_API_SERVER', 'https://api.' + DNS_DOMAIN + '/')
 TACC_JUPYTER_SERVER = os.environ.get(
-    'CATALOG_TACC_JUPYTER_SERVER', 'https://jupyter.' + DNS_DOMAIN)
+    'TACC_JUPYTER_SERVER', 'https://jupyter.' + DNS_DOMAIN)
 
 TACC_PRIMARY_STORAGE_SYSTEM = os.environ.get(
-    'CATALOG_TACC_PRIMARY_STORAGE_SYSTEM', 'data-sd2e-community')
+    'TACC_PRIMARY_STORAGE_SYSTEM', 'data-sd2e-community')
