72677f2108a829be5997f57b1d10d943a3f2c933
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-03-26 11:04:27 -0500

    Added indexes to samples, measurements collections. Added schema support for lab_sample_id. Added integration test for loading files with new sample ID formatting. Regenerated docs and schemas

--- tests/test_700_issues.py
+++ tests/test_700_issues.py
@@ -15,6 +15,10 @@ import traceback
 import transitions
 from datacatalog.identifiers import abaco, interestinganimal, typeduuid
 
+CWD = os.getcwd()
+HERE = os.path.dirname(os.path.abspath(__file__))
+PARENT = os.path.dirname(HERE)
+
 from .data import issues
 
 def test_issue15_create_large_job(mongodb_settings, agave, pipelinejobs_config, admin_token):
@@ -34,3 +38,13 @@ def test_issue15_create_large_job(mongodb_settings, agave, pipelinejobs_config, 
     mpj.setup()
     mpj.run()
     mpj.reset(token=admin_token)
+
+@longrun
+@pytest.mark.parametrize("filename", ['new-sample-id-scheme.json'])
+def test_pr80_sample_id_refactor_load_sampleset(mongodb_settings, filename):
+    DATA_DIR = os.path.join(HERE, 'data/sampleset')
+    jsonpath = os.path.join(DATA_DIR, filename)
+    db = datacatalog.managers.sampleset.SampleSetProcessor(mongodb_settings,
+                                                           samples_file=jsonpath)
+    dbp = db.process(strategy='merge')
+    assert dbp is True
