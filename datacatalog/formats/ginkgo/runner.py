df11487f3e803a276afb9efb38165c4d0cea520b
mweston <mark.william.weston@gmail.com>
2019-05-08 10:21:58 -0400

    Add support for Ginkgo PhiX parsing using in SmRNASeq experiments

--- datacatalog/formats/ginkgo/runner.py
+++ datacatalog/formats/ginkgo/runner.py
@@ -201,6 +201,18 @@ def convert_ginkgo(schema, encoding, input_file, verbose=True, output=True, outp
                 replicate_val = int(replicate_val_f)
             sample_doc[SampleConstants.REPLICATE] = replicate_val
 
+        # PhiX for SmallRNASeq
+        # "properties": {
+        #   "Library_prep_kit": "NextFlex_kit",
+        #    "PhiX": "10_percent"
+        # }
+        phix_prop = "PhiX"
+        if phix_prop in props:
+            phix_val = props[phix_prop]
+            if phix_val.endswith("_percent"):
+                phix_val = phix_val.replace("_percent", ":%")
+            contents.append(create_media_component(output_doc.get(SampleConstants.EXPERIMENT_ID, "not bound yet"), phix_prop, phix_prop, lab, sbh_query, phix_val))
+
         # record parent reference, if it exists
         parent_id_prop = "parent_id"
         if parent_id_prop in ginkgo_sample:
