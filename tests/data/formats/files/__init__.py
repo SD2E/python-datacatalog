a475220ed94892da5956ab44098112c668a87da5
mweston <mark.william.weston@gmail.com>
2019-06-05 16:58:46 -0400

    Fix test_200_json schemas test failure

--- tests/data/formats/files/__init__.py
+++ tests/data/formats/files/__init__.py
@@ -6,5 +6,6 @@ CLASSIFY = [('bio-15901_op82951.json', 'Biofab'),
             ('tx-r1c9pjzjs2c9k_r1c9wva2tpwj7.json', 'Transcriptic'),
             ('sa-r1bbktv6x4xke.json', 'SampleAttributes'),
             ('sa-r1bgtnn6nxy85.json', 'SampleAttributes'),
-            ('20181009-top-4-A-B-cell-variants-A-B-sampling-exp-1.xlsx', 'Caltech')
+            ('20181009-top-4-A-B-cell-variants-A-B-sampling-exp-1.xlsx', 'Caltech'),
+            ('20190214-A-B-mar-1.xlsx', 'Caltech')
             ]
