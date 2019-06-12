55c6d7bd91c746a740eeacd336c3d203a5b8c5dc
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-06-12 10:53:02 -0500

    admin_template() function should not have 'self' as a param

--- datacatalog/linkedstores/basestore/admin.py
+++ datacatalog/linkedstores/basestore/admin.py
@@ -1,6 +1,6 @@
 from ... import tenancy
 
-def admin_template(self):
+def admin_template():
     template = {'owner': tenancy.current_username(),
                 'project': tenancy.current_project(),
                 'tenant': tenancy.current_tenant()}
