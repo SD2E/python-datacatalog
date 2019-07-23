dedc6ce420aeecc2a13b8bf692169729b8bab160
mweston <mark.william.weston@gmail.com>
2019-07-23 10:21:42 -0400

    Schema update for new Biofab traces

--- tests/data/formats/files/__init__.py
+++ tests/data/formats/files/__init__.py
@@ -11,5 +11,6 @@ CLASSIFY = [('bio-15901_op82951.json', 'Biofab', projects.SD2.tenant, projects.S
             ('sa-r1bgtnn6nxy85.json', 'SampleAttributes', projects.SD2.tenant, projects.SD2.tacc_name),
             ('20181009-top-4-A-B-cell-variants-A-B-sampling-exp-1.xlsx', 'Caltech', projects.SD2.tenant, projects.BIOCON.tacc_name),
             ('20190214-A-B-mar-1.xlsx', 'Caltech', projects.SD2.tenant, projects.BIOCON.tacc_name),
-            ('TACC_genomics_metadata.xlsx', 'Marshall', projects.SAFEGENES.tenant, projects.SAFEGENES.tacc_name)
+            ('TACC_genomics_metadata.xlsx', 'Marshall', projects.SAFEGENES.tenant, projects.SAFEGENES.tacc_name),
+            ('34206.json', 'Biofab', projects.SD2.tenant, projects.SD2.tacc_name)
             ]
