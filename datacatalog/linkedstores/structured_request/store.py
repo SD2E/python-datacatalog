d0a2def049f8b5e44dc89b55774498c25a3727c6
gzheng2 <gzheng@eagc.test>
2019-09-23 16:29:40 -0400

    added status parent for etl jobs

--- datacatalog/linkedstores/structured_request/store.py
+++ datacatalog/linkedstores/structured_request/store.py
@@ -29,16 +29,25 @@ class StructuredRequestStore(SoftDelete, LinkedStore):
         self.add_update_document(structured_request, strategy=strategies.REPLACE)   
 
     def update_request_status_for_etl(self, experiment_id, key, job_dict): 
+        print("update_request_status_for_etl for experiment_id: {}".format(experiment_id))
         query = {"experiment_id": experiment_id}
         matches = self.query(query)        
         
         # There should be at most one match
         if matches.count() == 0:
+            print("SR not found")
             return False
         else:
             structured_request = matches[0]
-            
-        structured_request[key] = job_dict
+        
+        etl_jobs = structured_request["status"][key]
+        if etl_jobs is None:
+            print("adding stub")
+            structured_request["status"][key] = {}
+        else:
+            print("appending job_dict")
+            etl_jobs.append(job_dict)
+
         self.add_update_document(structured_request, strategy=strategies.REPLACE)
         
         return True
