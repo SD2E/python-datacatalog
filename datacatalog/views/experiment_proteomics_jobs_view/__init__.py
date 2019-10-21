fb4ecc9f12c443197b53a2e134af53b2dae95134
Ethan Ho <eho@tacc.utexas.edu>
2019-10-21 10:11:31 -0500

    Edit view metadata

--- datacatalog/views/experiment_proteomics_jobs_view/__init__.py
+++ datacatalog/views/experiment_proteomics_jobs_view/__init__.py
@@ -1,3 +1,3 @@
 AUTHOR = 'eho@tacc.utexas.edu'
 DESCRIPTION = 'Experiment with proteomics jobs completed'
-# MONGODB_VIEW_NAME = 'experiment_rnaseq_jobs_view'
+# MONGODB_VIEW_NAME = 'experiment_proteomics_jobs_view'
