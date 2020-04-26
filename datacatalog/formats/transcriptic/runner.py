b4a6850479a26a339eb8f51b2a6be704a90344f5
mweston <mark.william.weston@gmail.com>
2020-04-26 13:21:21 -0400

    Hotfix for Strateos #2: fill in missing bead markers

--- datacatalog/formats/transcriptic/runner.py
+++ datacatalog/formats/transcriptic/runner.py
@@ -284,6 +284,9 @@ def convert_transcriptic(schema, encoding, input_file, verbose=True, output=True
             if strain == "SpheroControl1":
                 strain = "SpheroControl"
 
+            if strain == "SpheroControl" and SampleConstants.STANDARD_TYPE not in transcriptic_sample:
+                sample_doc[SampleConstants.STANDARD_TYPE] = SampleConstants.STANDARD_BEAD_FLUORESCENCE
+
             # TX does not mark size beads consistently
             if strain == "SizeBeadControl":
                 sample_doc[SampleConstants.STANDARD_TYPE] = SampleConstants.STANDARD_BEAD_SIZE
