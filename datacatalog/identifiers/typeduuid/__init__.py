72aed44152fcb276b039e203e2ed2c37377fe184
Matt Vaughn <vaughn@tacc.utexas.edu>
2020-08-26 16:18:52 -0500

    Add example typed UUID to submodule documentation

--- datacatalog/identifiers/typeduuid/__init__.py
+++ datacatalog/identifiers/typeduuid/__init__.py
@@ -2,7 +2,7 @@
 
 A typed UUID is a UUID5 generated from one or more plaintext 
 string identifiers. Each managed document has a typed 
-UUID field 'uuid`. 
+UUID field 'uuid'. 
 
 These are called 'typed UUID' because the three-digit prefix 
 for a given UUID denotes which managed document type it refers 
@@ -17,6 +17,14 @@ In addition to defining UUID types, other functions of
 this submodule include operations to generate JSONschema 
 definitions for each type, create and validate typed UUIDs, 
 convert to other UUID formats such as HashId. 
+
+A example challenge problem typed UUID is 
+'101516ac-9549-5ca1-bc94-6fe0b80094b'. This UUID validates 
+as a UUID5 but also the initial three digits '101' indicate 
+that it refers to a 'challenge_problem' document. By way of 
+the 'generate' function in this module, 
+'101516ac-9549-5ca1-bc94-6fe0b80094b' maps to the human-
+readable identifier 'PROTEIN_DESIGN'
 """
 
 from .schemas import get_schemas
