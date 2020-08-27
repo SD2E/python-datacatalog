7d01244d40b9af6f727eba67e805e27a829f5bb5
Matt Vaughn <vaughn@tacc.utexas.edu>
2020-08-27 09:56:05 -0500

    Doc update

--- datacatalog/identifiers/typeduuid/examples.py
+++ datacatalog/identifiers/typeduuid/examples.py
@@ -1,1 +1,3 @@
+"""Defines one or more globals containing example Typed UUIDs
+"""
 TYPEDUUID = ['1010f74e-cd69-51de-b5a1-1570c88c01f1', '114d6d85-6dab-54af-8fee-5d7871df2a55', '102edd93-29d6-5483-b60b-8dfd4d094b9c']
