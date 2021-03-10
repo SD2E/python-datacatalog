b6634beff76de70128641f68c4a3dd6bb79a10d2
gzheng2 <gzheng@eagc.test>
2021-03-10 10:46:12 -0500

    added agave argument so it can be passed in from invoking reactor

--- scripts/tapis_job_cleanup.py
+++ scripts/tapis_job_cleanup.py
@@ -2,10 +2,11 @@ import pymongo
 import sys
 from agavepy.agave import Agave
 
-def get_tapis_jobs_for_experiment(db_uri, ex_id):
+def get_tapis_jobs_for_experiment(db_uri, ex_id, agave=None):
     print(f"db_uri: {db_uri} ex_id: {db_uri}")
     client = pymongo.MongoClient(db_uri)
-    agave = Agave.restore()
+    if agave is None:
+        agave = Agave.restore()
     
     # The view runs only RUNNING pipeline jobs
     etjv = client.catalog_staging.experiment_tapis_jobs_view
@@ -28,14 +29,14 @@ def get_tapis_jobs_for_experiment(db_uri, ex_id):
                     print(f"exc: {exc}")
     return jobs_map
 
-def get_tapis_jobs_for_experiment_reference(db_uri, ex_ref):
+def get_tapis_jobs_for_experiment_reference(db_uri, ex_ref, agave=None):
     client = pymongo.MongoClient(db_uri)
     sr = client.catalog_staging.structured_requests
     matches = sr.find({"experiment_reference":ex_ref})
     jobs_map = {}
     if matches.count() == 1: # no children
         ex = matches[0]
-        jobs_map = get_tapis_jobs_for_experiment(db_uri, ex["experiment_id"])
+        jobs_map = get_tapis_jobs_for_experiment(db_uri, ex["experiment_id"], agave)
     else:
         for ex in matches:
             jm = get_tapis_jobs_for_experiment(db_uri, ex["experiment_id"])
@@ -64,9 +65,10 @@ def main():
         sys.exit(2)
 
     print(sys.argv[1])
-    jobs_map = get_tapis_jobs_for_experiment_reference(sys.argv[1], "NovelChassis-Endogenous-Promoter")
+    #jobs_map = get_tapis_jobs_for_experiment_reference(sys.argv[1], "NovelChassis-Endogenous-Promoter")
+    jobs_map = get_tapis_jobs_for_experiment_reference(sys.argv[1], "YeastSTATES-Dual-Response-CRISPR-Short-Duration-Time-Series-30C", Agave.restore())
     print(f"jobs_map: {jobs_map}")
-    stop_tapis_jobs(jobs_map, ["Precomputed data table"])
+    #stop_tapis_jobs(jobs_map, ["Precomputed data table"])
   
 if __name__ == '__main__':
   main() 
