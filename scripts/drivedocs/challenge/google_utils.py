5c4f508b9e2cebb5f219ecba04c85a37916149e0
mweston <mark.william.weston@gmail.com>
2021-10-12 11:18:45 -0400

    Update google-api-python-client for 2.25.0 - enable self signed jwt for service account credentials causing issues with authorization. Turn this new keyword off (default is True) to allow builds/google drive API to continue to work

--- scripts/drivedocs/challenge/google_utils.py
+++ scripts/drivedocs/challenge/google_utils.py
@@ -27,7 +27,7 @@ def get_drive_service(credential_path=SERVICE_ACCOUNT_FILE, scopes=SCOPES):
     credentials = service_account.Credentials.from_service_account_file(
         credential_path, scopes=scopes)
     service = googleapiclient.discovery.build(
-        'drive', 'v3', credentials=credentials)
+        'drive', 'v3', credentials=credentials, always_use_jwt_access=False)
     return service
 
 def get_folders(filename, folder_id, drive_service=None):
